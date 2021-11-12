73f7a97caf971d1d2341a1639404f83e5c1f0e81
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-12 10:08:26 +0100

    feat(screens/pokelistscreen/): add poke list screen and its own index and style

---
+++ src/screens/PokeListScreen/PokeListScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {ScreenContainer} from '../../components';
+
+const PokeListScreen: React.FC = () => {
+  return (
+    <ScreenContainer>
+      <Text>Welcome to poke list</Text>
+    </ScreenContainer>
+  );
+};
+
+export default PokeListScreen;

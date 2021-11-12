ff1a6c95523f6d9cb543e24f3a5a09256a408bad
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-12 10:07:48 +0100

    feat(screens/profilescreen/): add profile screen and its own index and style

---
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {ScreenContainer} from '../../components';
+
+const ProfileScreen: React.FC = () => {
+  return (
+    <ScreenContainer>
+      <Text>Welcome to profile screen</Text>
+    </ScreenContainer>
+  );
+};
+
+export default ProfileScreen;

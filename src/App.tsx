695f4469bd9e637d0e197abff3c0b417e3cad3bb
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-12 01:27:56 +0100

    feat(*): create new application. Create all the needed folders and files to start working on the app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Start the app.</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

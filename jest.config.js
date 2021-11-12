d988e23de14ea466de5fa30b89b0123c9a821d46
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-12 17:31:45 +0100

    feat(jest.config.js): include newly made testing files in modules of Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/testing/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/testing/styleMock.js',
+  },
 };

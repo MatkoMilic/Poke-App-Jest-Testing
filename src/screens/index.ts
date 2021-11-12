71b58c2c1791d06fd67b67070afdfab40b2554b7
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-12 10:11:17 +0100

    refactor(components/index screens/index): update indexes according to other newly created indexes

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,5 @@
-export {}
+export {LoginScreen} from './LoginScreen';
+export {LoadingScreen} from './LoadingScreen';
+export {PokeListScreen} from './PokeListScreen';
+export {ProfileScreen} from './ProfileScreen';
+export {SettingsScreen} from './SettingsScreen';

7f08fbdba9ee880d8a71a3df4c2fd52dc3c0d493
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-13 21:33:31 +0100

    feat(loadingscreen): use switch and use context

--- src/screens/LoadingScreen/LoadingScreen.tsx
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {Text} from 'react-native';
-import {ScreenContainer} from '../../components';
+import {Switch, useTheme} from 'react-native-paper';
+import {ScreenContainer, ThemeContext} from '../../components';
+import style from './styles';
 
-const LoadingScreen: React.FC = () => {
+const LoadingScreen: FC = () => {
+  const theme = useTheme();
+  const {toggleTheme, isThemeDark} = React.useContext(ThemeContext);
+
   return (
     <ScreenContainer>
+      <Switch
+        style={style.switchElement}
+        color={theme.colors.text}
+        value={isThemeDark}
+        onValueChange={toggleTheme}
+      />
       <Text>Loading</Text>
     </ScreenContainer>
   );

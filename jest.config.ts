b86bf239e5a773f4f528610919147dc2ad020d70
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-15 17:44:45 +0100

    refactor(jest.config.ts): make jest config a TS file and not JS file

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export {};
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/testing/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/testing/styleMock.ts',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+};

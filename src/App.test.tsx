d8af103a552ef4a3ef841437920bf2365f077b5d
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-16 18:04:12 +0100

    fix(app.test.tsx): fix bug where jest doesnt function like async but some parts of code do

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
+import {act} from '@testing-library/react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders correctly', () => {
+test('renders correctly', async () => {
+  await act(async () => {});
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });

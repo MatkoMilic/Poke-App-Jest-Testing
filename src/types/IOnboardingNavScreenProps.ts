a1dc783a266d96d886b5015de0c9198e85e1e44e
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-16 17:58:21 +0100

    feat(types/ionboardingnavscreenprops): add needed screen props for Onboarding screens to use nav

---
+++ src/types/IOnboardingNavScreenProps.ts
@@ -0,0 +1,15 @@
+import {CompositeNavigationProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {
+  OnboardingNavigatorParamList,
+  RootNavigatorParamList,
+  RootNavigatorType,
+} from './navigatorTypes';
+
+export type OnboardingNavigationType = CompositeNavigationProp<
+  NativeStackNavigationProp<OnboardingNavigatorParamList>,
+  NativeStackNavigationProp<RootNavigatorType>
+>;
+export interface IOnboardingNavScreenProps {
+  navigation: OnboardingNavigationType;
+}

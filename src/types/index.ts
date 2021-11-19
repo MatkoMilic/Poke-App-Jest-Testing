8750ea0d3cd29e16cb079a3928350481a3e88b15
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-19 17:34:15 +0100

    refactor: add indexes in different folders

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export type {
+  IMainNavScreenProps,
+  MainNavigationType,
+} from './IMainNavScreenProps';
+export type {
+  IOnboardingNavScreenProps,
+  OnboardingNavigationType,
+} from './IOnboardingNavScreenProps';
+export type {ReactWrapperType, RenderApiType} from './testingTypes';
+export type {
+  MainNavigatorKeys,
+  NavigatorNameKeys,
+  OnboardingNavigatorParamList,
+  RootNavigatorKeys,
+  RootNavigatorParamList,
+  OnboardingNavigatorKeys,
+  MainNavigatorParamList,
+  RootNavigatorType,
+} from './navigatorTypes';
+export {
+  NavigatorNames,
+  OnboardingNavigatorScreens,
+  MainNavigatorScreens,
+} from './navigatorTypes';
---
+++ src/types/testingTypes/index.ts
@@ -0,0 +1,1 @@
+export type {ReactWrapperType, RenderApiType} from './testingTypes';

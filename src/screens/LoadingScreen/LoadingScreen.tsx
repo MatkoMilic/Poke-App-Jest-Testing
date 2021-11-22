b3d1bc0baa63673620a8136d88270193e59638b7
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-22 17:41:53 +0100

    feat(loadingscreen.tsx): add custom header and button to navigate to profile screen

--- src/screens/LoadingScreen/LoadingScreen.tsx
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,16 +1,25 @@
 import React, {FC} from 'react';
-import {Switch, Text, TouchableOpacity} from 'react-native';
+import {Button, Switch, Text, TouchableOpacity} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ThemeContext} from '../../components';
 import style from './styles';
-import {IOnboardingNavScreenProps} from '../../types';
+import {
+  IOnboardingNavScreenProps,
+  MainNavigatorScreens,
+  NavigatorNames,
+} from '../../types';
 
 interface LoadingScreenProps extends IOnboardingNavScreenProps {}
 
-const LoadingScreen: FC = () => {
+const LoadingScreen: FC<LoadingScreenProps> = ({navigation}) => {
   const theme = useTheme();
   const {toggleTheme, isThemeDark} = React.useContext(ThemeContext);
+  const goToProfile = () => {
+    navigation.navigate(NavigatorNames.MAIN_NAVIGATOR, {
+      screen: MainNavigatorScreens.PROFILE_SCREEN,
+    });
+  };
   return (
     <SafeAreaView>
       <TouchableOpacity>
@@ -23,6 +32,7 @@ const LoadingScreen: FC = () => {
       <Text style={{color: 'red'}}>
         Loading & theme is: {isThemeDark ? 'dark' : 'light'}
       </Text>
+      <Button title="Go To Profile Screen" onPress={goToProfile}></Button>
     </SafeAreaView>
   );
 };

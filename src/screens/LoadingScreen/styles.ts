4cb243e49000296686e79d238f97500b0ad3a2f5
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-23 22:53:08 +0100

    style(settingsscreen/styles.ts & loadingscreen/styles.ts): exchange styles between these 2 screens

--- src/screens/LoadingScreen/styles.ts
+++ src/screens/LoadingScreen/styles.ts
@@ -1,25 +1,3 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({
-  switchView: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  switchElement: {
-    borderRadius: 15,
-  },
-  headerContent: {
-    alignItems: 'center',
-  },
-  deleteButton: {
-    color: 'white',
-    fontWeight: '500',
-    fontSize: 25,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    textAlign: 'center',
-    borderRadius: 20,
-    padding: 20,
-    margin: 10,
-  },
-});
+export default StyleSheet.create({});

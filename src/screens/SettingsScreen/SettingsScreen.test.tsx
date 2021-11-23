26630521e6439a880e04c65b113ed2bfe76eedb1
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-23 16:09:28 +0100

    feat(settingsscreen.test.tsx): add missing property to the settings screen testing - navigation

--- src/screens/SettingsScreen/SettingsScreen.test.tsx
+++ src/screens/SettingsScreen/SettingsScreen.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {MainNavigationType} from '../../types';
 import SettingsScreen from './SettingsScreen';
 
+let navigation: MainNavigationType;
+
 test('renders correctly', () => {
-  const tree = renderer.create(<SettingsScreen />).toJSON();
+  const tree = renderer
+    .create(<SettingsScreen navigation={navigation} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });

759d0c5004a53b07ce065510e83df25267026c3e
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:10:01 +0100

    style(loadingscreen/styles.ts): add styles for elements in loading screen

--- src/screens/LoadingScreen/styles.ts
+++ src/screens/LoadingScreen/styles.ts
@@ -1,3 +1,13 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  titleView: {
+    flex: 1,
+    padding: 24,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  titleText: {
+    padding: 10,
+  },
+});

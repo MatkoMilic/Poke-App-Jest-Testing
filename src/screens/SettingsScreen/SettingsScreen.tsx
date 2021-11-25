1577906e407978efe29f752921e2c246ff97cc05
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:56:15 +0100

    feat(settingsscreen.tsx): add logout button and change icons to match functions

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Text, TouchableOpacity, Switch} from 'react-native';
-import {useTheme} from 'react-native-paper';
+import {Button, useTheme} from 'react-native-paper';
 import {Header, ScreenContainer, ThemeContext} from '../../components';
-import {IMainNavScreenProps, MainNavigatorScreens} from '../../types';
+import {removeActiveUser} from '../../constants';
+import {
+  IMainNavScreenProps,
+  MainNavigatorScreens,
+  NavigatorNames,
+  OnboardingNavigatorScreens,
+} from '../../types';
 import styles from './styles';
 
 interface SettingsScreenProps extends IMainNavScreenProps {}
@@ -17,15 +23,22 @@ const SettingsScreen: React.FC<SettingsScreenProps> = ({navigation}) => {
   const theme = useTheme();
   const {toggleTheme, isThemeDark} = React.useContext(ThemeContext);
 
+  const logoutUser = async () => {
+    removeActiveUser();
+    navigation.replace(NavigatorNames.ONBOARDING_NAVIGATOR, {
+      screen: OnboardingNavigatorScreens.LOGIN_SCREEN,
+    });
+  };
+
   return (
     <ScreenContainer>
       <Header
         headerTitle="Poke Settings"
-        leftIcon="clipboard-list"
-        rightIcon="account-cog"
+        leftIcon="home-account"
+        rightIcon="clipboard-list"
         headerSubtitle="2front"
-        leftOnPress={goToPokeList}
-        rightOnPress={goToProfile}
+        leftOnPress={goToProfile}
+        rightOnPress={goToPokeList}
       />
       <TouchableOpacity>
         <Switch
@@ -39,6 +52,7 @@ const SettingsScreen: React.FC<SettingsScreenProps> = ({navigation}) => {
         {`Theme at the moment is: ${isThemeDark.toString()}`}
       </Text>
       <Text>Welcome to settings</Text>
+      <Button onPress={logoutUser}>LOG OUT</Button>
     </ScreenContainer>
   );
 };

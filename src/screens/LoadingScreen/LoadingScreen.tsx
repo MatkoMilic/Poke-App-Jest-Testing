1432e4cd0878e99740ee5de75632aaa5e1dcf56e
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:10:54 +0100

    feat(loadingscreen.tsx): add routing logic inside loading screen, use different functions for it

--- src/screens/LoadingScreen/LoadingScreen.tsx
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,25 +1,45 @@
-import React, {FC} from 'react';
-import {Button, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from './styles';
+import {StatusBar, ScreenContainer} from '../../components';
 import {
   IOnboardingNavScreenProps,
   MainNavigatorScreens,
   NavigatorNames,
+  OnboardingNavigatorScreens,
 } from '../../types';
 
 interface LoadingScreenProps extends IOnboardingNavScreenProps {}
 
-const LoadingScreen: FC<LoadingScreenProps> = ({navigation}) => {
-  const goToSettings = () => {
-    navigation.navigate(NavigatorNames.MAIN_NAVIGATOR, {
-      screen: MainNavigatorScreens.SETTINGS_SCREEN,
-    });
+const LoadingScreen: React.FC<LoadingScreenProps> = ({navigation}) => {
+  const theme = useTheme();
+
+  const chooseNavigator = async () => {
+    const isUserLoggedIn = await AsyncStorage.getItem('activeUser');
+    if (isUserLoggedIn !== null) {
+      navigation.replace(NavigatorNames.MAIN_NAVIGATOR, {
+        screen: MainNavigatorScreens.PROFILE_SCREEN,
+      });
+    } else {
+      navigation.replace(OnboardingNavigatorScreens.LOGIN_SCREEN);
+    }
   };
+
+  useEffect(() => {
+    chooseNavigator();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Button title="Go To Profile Screen" onPress={goToSettings}></Button>
-      <Text testID={'loadingText'}>Loading...please wait.</Text>
-    </SafeAreaView>
+    <ScreenContainer>
+      <StatusBar />
+      <View style={styles.titleView}>
+        <Text style={[{color: theme.colors.text}, styles.titleText]}>
+          Loading...please wait
+        </Text>
+      </View>
+    </ScreenContainer>
   );
 };
 

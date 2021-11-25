7204984dbcf37681a7b233279a93bf31c7f451e2
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 13:37:00 +0100

    feat(header.test.tsx): add tests for header, mainly about props

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -9,7 +9,9 @@ import renderer from 'react-test-renderer';
 import {MainNavigationType, MainNavigatorScreens} from '../../types';
 configure({adapter: new Adapter()});
 
-const goSettingsFunc = jest.fn();
+const leftPressMock = jest.fn();
+const rightPressMock = jest.fn();
+
 let navigation: MainNavigationType;
 const goToSettings = () => {
   navigation.navigate(MainNavigatorScreens.SETTINGS_SCREEN);
@@ -17,10 +19,20 @@ const goToSettings = () => {
 
 describe('Header', () => {
   let renderHeader: ReactTestRendererJSON | ReactTestRendererJSON[] | null;
+  let mountHeaderLeftPress: any;
+  let mountHeaderRightPress: any;
+  let mountHeaderTitle: any;
+  let mountLeftIcon: any;
+  let mountRightIcon: any;
   let mountHeader: any;
   let createHeader: ReactTestRendererJSON | ReactTestRendererJSON[] | null;
   beforeEach(() => {
-    mountHeader = mount(<Header leftOnPress={goSettingsFunc} />);
+    mountHeaderLeftPress = mount(<Header leftOnPress={leftPressMock} />);
+    mountHeaderRightPress = mount(<Header rightOnPress={rightPressMock} />);
+    mountLeftIcon = mount(<Header leftIcon={'TestWithWrongLeftIconName'} />);
+    mountRightIcon = mount(<Header rightIcon={'TestWithWrongRightIconName'} />);
+    mountHeaderTitle = mount(<Header headerTitle={'TestTitle'} />);
+    mountHeader = mount(<Header />);
     renderHeader = render(<Header></Header>).toJSON();
     createHeader = renderer
       .create(
@@ -33,6 +45,7 @@ describe('Header', () => {
         />,
       )
       .toJSON();
+
     expect(createHeader).toMatchSnapshot();
   });
   it('does header mount', async () => {
@@ -48,8 +61,52 @@ describe('Header', () => {
   it('does header show children', () => {
     expect(createHeader).toMatchSnapshot();
   });
-  it('does onPress work', () => {
-    mountHeader.find(Header).first().props().leftOnPress!();
-    expect(goSettingsFunc).toHaveBeenCalled();
+  it('check is leftOnPress called inside Header', () => {
+    mountHeaderLeftPress.find(Header).first().props().leftOnPress();
+    expect(leftPressMock).toHaveBeenCalled();
+  });
+  it('check is rightOnPress called inside Header', () => {
+    mountHeaderRightPress.find(Header).first().props().rightOnPress();
+    expect(rightPressMock).toHaveBeenCalled();
+  });
+  it('test does header render only with title and without other props', () => {
+    expect(mountHeaderTitle.props().headerTitle).toBeDefined();
+    expect(mountHeaderTitle.props().leftOnPress).toBeUndefined();
+    expect(mountHeaderTitle.props().rightOnPress).toBeUndefined();
+    expect(mountHeaderTitle.props().leftIcon).toBeUndefined();
+    expect(mountHeaderTitle.props().rightIcon).toBeUndefined();
+    expect(mountHeaderTitle).toBeTruthy();
+  });
+  it('test does header render only with leftOnPress and without other props', () => {
+    expect(mountHeaderLeftPress.props().leftOnPress).toBeDefined();
+    expect(mountHeaderLeftPress.props().headerTitle).toBeUndefined();
+    expect(mountHeaderLeftPress.props().rightOnPress).toBeUndefined();
+    expect(mountHeaderLeftPress.props().leftIcon).toBeUndefined();
+    expect(mountHeaderLeftPress.props().rightIcon).toBeUndefined();
+    expect(mountHeaderLeftPress).toBeTruthy();
+  });
+  it('test does header render only with rightOnPress and without other props', () => {
+    expect(mountHeaderRightPress.props().rightOnPress).toBeDefined();
+    expect(mountHeaderRightPress.props().headerTitle).toBeUndefined();
+    expect(mountHeaderRightPress.props().leftOnPress).toBeUndefined();
+    expect(mountHeaderRightPress.props().leftIcon).toBeUndefined();
+    expect(mountHeaderRightPress.props().rightIcon).toBeUndefined();
+    expect(mountHeaderRightPress).toBeTruthy();
+  });
+  it('test does header render only with leftIcon and without other props', () => {
+    expect(mountLeftIcon.props().leftIcon).toBeDefined();
+    expect(mountLeftIcon.props().leftOnPress).toBeUndefined();
+    expect(mountLeftIcon.props().rightOnPress).toBeUndefined();
+    expect(mountLeftIcon.props().headerTitle).toBeUndefined();
+    expect(mountLeftIcon.props().rightIcon).toBeUndefined();
+    expect(mountLeftIcon).toBeTruthy();
+  });
+  it('test does header render only with rightIcon and without other props', () => {
+    expect(mountRightIcon.props().rightIcon).toBeDefined();
+    expect(mountRightIcon.props().leftOnPress).toBeUndefined();
+    expect(mountRightIcon.props().rightOnPress).toBeUndefined();
+    expect(mountRightIcon.props().headerTitle).toBeUndefined();
+    expect(mountRightIcon.props().leftIcon).toBeUndefined();
+    expect(mountRightIcon).toBeTruthy();
   });
 });

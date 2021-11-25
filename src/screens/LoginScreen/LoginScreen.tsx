136e2994f08b67c3ab00e0d4b80c40a75c6e7047
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:18:20 +0100

    feat(loginscreen.tsx): add login form to login screen

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {ScreenContainer} from '../../components';
+import {Text, View} from 'react-native';
+import {Formik} from 'formik';
+import {LoginForm, ScreenContainer} from '../../components';
+import {loginSchema} from '../../validation';
+import {IOnboardingNavScreenProps} from '../../types';
+import {useOnSubmit} from '../../utils';
 
-const LoginScreen: React.FC = () => {
+interface LoginScreenProps extends IOnboardingNavScreenProps {}
+
+const LoginScreen: React.FC<LoginScreenProps> = ({navigation}) => {
+  const {onSubmit} = useOnSubmit(navigation);
+
   return (
     <ScreenContainer>
       <Text>Welcome to login</Text>
+      <View>
+        <Formik
+          initialValues={{email: '', password: ''}}
+          onSubmit={(values) => {
+            onSubmit(values.email, values.password);
+          }}
+          validationSchema={loginSchema}
+          component={LoginForm}
+        />
+      </View>
     </ScreenContainer>
   );
 };

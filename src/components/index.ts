27c768258bba4c7d33fc287b025310ef90d41881
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:58:53 +0100

    refactor(components/index.ts): update index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export {ScreenContainer} from './ScreenContainer';
 export {ThemeProvider, ThemeContext} from './ThemeProvider';
 export {NavigationProvider} from './NavigationProvider';
 export {Header} from './Header';
+export {InputField} from './InputField';
+export {LoginForm} from './LoginForm';
+export {StatusBar} from './StatusBar';

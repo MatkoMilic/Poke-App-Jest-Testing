34abf90e9721c9517703885a01da9683754ae5ab
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-25 16:19:35 +0100

    feat(loginscreen.test.tsx): add login screen testing nvigation mocked

--- src/screens/LoginScreen/LoginScreen.test.tsx
+++ src/screens/LoginScreen/LoginScreen.test.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {OnboardingNavigationType} from '../../types';
 import LoginScreen from './LoginScreen';
 
+let navigation: Partial<OnboardingNavigationType>;
+beforeEach(() => {
+  navigation = {
+    dispatch: jest.fn(),
+  };
+});
 test('login screen renders correctly', () => {
-  const tree = renderer.create(<LoginScreen />).toJSON();
+  const tree = renderer
+    .create(<LoginScreen navigation={navigation as OnboardingNavigationType} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });

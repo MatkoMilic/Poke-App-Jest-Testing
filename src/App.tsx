f185ce3ae5db71e03ec7e0c368d9776ea3446f82
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-30 14:14:59 +0100

    feat(src/app.tsx): add new provider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {RootNavigator} from './navigators';
 import {NavigationProvider, ThemeProvider} from './components';
+import {UserDetailsProvider} from './components/UserDetailsProvider';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
-      <NavigationProvider>
-        <RootNavigator />
-      </NavigationProvider>
+      <UserDetailsProvider>
+        <NavigationProvider>
+          <RootNavigator />
+        </NavigationProvider>
+      </UserDetailsProvider>
     </ThemeProvider>
   );
 };

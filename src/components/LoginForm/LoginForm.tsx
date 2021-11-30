f0f2b3f1526dfc45614035ca3bd4589e70781caa
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-11-30 13:41:03 +0100

    feat(loginform/loginform.tsx): add changes to LoginForm for testing goals

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {FormikProps} from 'formik';
+import {Formik, FormikProps} from 'formik';
 import {Button} from 'react-native-paper';
 import {InputField} from '../InputField';
 import {styles} from './styles';
+import {useOnSubmit} from '../../utils';
+import {IOnboardingNavScreenProps} from '../../types';
+import {loginSchema} from '../../validation';
 
 interface FormikValues {
   email: string;
   password: string;
 }
+type PickFormikProps = Pick<
+  FormikProps<FormikValues>,
+  'handleChange' | 'errors' | 'values' | 'handleSubmit'
+>;
+interface LoginScreenProps extends IOnboardingNavScreenProps {}
+//interface ICombinedInterface extends LoginScreenProps & PickFormikProps;
+//const {onSubmit} = useOnSubmit(navigation);
 
-const LoginForm: React.FC<FormikProps<FormikValues>> = ({
+const LoginForm: React.FC<PickFormikProps> = ({
   handleChange,
   errors,
   values,
@@ -19,17 +29,27 @@ const LoginForm: React.FC<FormikProps<FormikValues>> = ({
   return (
     <View>
       <InputField
+        testID="InputFieldEmail"
         placeholder="Your email please"
         onChangeText={handleChange('email')}
-        value={values.email}></InputField>
-      <Text style={styles.inputErrorMessage}>{errors.email}</Text>
+        value={values.email}
+      />
+      <Text testID={'errorEmail'} style={styles.inputErrorMessage}>
+        {errors.email}
+      </Text>
       <InputField
+        testID="InputFieldPassword"
         placeholder="Your password please"
         secureTextEntry
         onChangeText={handleChange('password')}
-        value={values.password}></InputField>
+        value={values.password}
+      />
       <Text style={styles.inputErrorMessage}>{errors.password}</Text>
-      <Button icon="account-arrow-right" color={'red'} onPress={handleSubmit}>
+      <Button
+        testID="buttonHandleSubmit"
+        icon="account-arrow-right"
+        color={'red'}
+        onPress={handleSubmit}>
         Login now
       </Button>
     </View>

3a61ef3d79858c8f7aa7f88f84cc5babeff0fda0
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-01 11:12:50 +0100

    refactor(pokelistscreen.test.tsx): add a todo comment for abort controller issue

--- src/screens/PokeListScreen/PokeListScreen.test.tsx
+++ src/screens/PokeListScreen/PokeListScreen.test.tsx
@@ -10,6 +10,8 @@ beforeEach(() => {
   };
 });
 
+//TODO: this test wont work because pokelist screen
+//contains abort controller which jest TS currently doesnt support
 test('pokelist screen renders correctly', () => {
   const tree = renderer
     .create(<PokeListScreen navigation={navigation as MainNavigationType} />)
